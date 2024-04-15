cb507391742f756b6263d038c375f026cce11be9
Alireza Akbarzadeh <Akbarzadeh.Ali@hasti.co>
2024-04-15 12:56:08 +0330

    feat: Implement Mockbin API for fetching flight data

---
+++ constant/endpoint.ts
@@ -0,0 +1,9 @@
+// Welcome to Mockbin by Zuplo
+/**
+ * This is an open-source and fully-free tool that allows you to quickly mock an API endpoint, configure a fixed response and track requests to your endpoint.
+ *
+ *
+ * @see https://mockbin.io
+ */
+
+export const endpoint: string = "https://c2d1585c64f9489ea35fca71730905a6.api.mockbin.io/"
