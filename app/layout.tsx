421626c8e473ae81434da1775d4459e4e706944b
Alireza Akbarzadeh <Akbarzadeh.Ali@hasti.co>
2024-04-15 12:44:32 +0330

    refactor: format with prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={cn("bg-background min-h-screen font-sans antialiased", fontSans.variable)}>{children}</body>
+      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>{children}</body>
     </html>
   )
 }
